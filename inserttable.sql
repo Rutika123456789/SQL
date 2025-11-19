0f65567b5a89955dc43ef045996ba66c152cdb03
Rutika123456789 <karalerutika16@gmail.com>
2025-11-19 23:11:02 +0530

    Second day commitse

--- inserttable.sql
+++ inserttable.sql
@@ -1,5 +1,7 @@
 INSERT INTO students(id,name,age)
-VALUES (1."Rutika" ,21),
+VALUES   (1."Rutika" ,21),
          (2,"Manisha" ,22),
-         (3,"Ankita" ,20);  
+         (3,"Ankita" ,20); 
+         (4,"Shravani",22),
+         (5,"Sakshi" ,20);  
          
