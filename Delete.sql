fa9fc0dcacd34dbd7548fd9b8dd42e014e27e733
Rutika123456789 <karalerutika16@gmail.com>
2025-11-20 23:49:00 +0530

    3rd day

--- Delete.sql
+++ Delete.sql
@@ -1,5 +1,5 @@
 
------ DELETE Statements-------------------
+----- DELETE Statements----------------------
 DELETE From students WHERE id=1;
 DELETE FROM students WHERE name = 'Rutika';
 DELETE FROM students WHERE age = 20;
