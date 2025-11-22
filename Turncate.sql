72b590d100ac99f6ded84906d373d20563c21473
Rutika123456789 <karalerutika16@gmail.com>
2025-11-22 23:13:29 +0530

    4th Sql Practice

---
+++ Turncate.sql
@@ -0,0 +1,25 @@
+--------------- 1 Combined two rows with the help of left join 
+   
+    SELECT e.first_name, d.department_name FROM employees e
+    LEFT JOIN departments d ON e.department_id = d.department_id;
+
+
+
+--------------- 2 Retriever specific column from the table 
+
+    SELECT First_name, Last_name From the employees;
+
+
+--------------- 3 Write a query to retrieve Rose with dates with in specific range 
+
+
+
+To combine the result state of two or more select statement 
+
+
+
+
+
+
+Creative table employees with fields like empld empage  empaddress 
+empsalary
